70ad8d6e5a143d87d2881b8564a03b35a8307e17
Alan Dugger <adugger1@my.westga.edu>
2020-03-27 17:07:33 -0400

    Added model test for name/location validation

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -4,4 +4,11 @@ class RestaurantTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "restaurant name and location must be present" do
+    restaurant = Restaurant.new
+    assert restaurant.invalid?
+    assert restaurant.errors[:name].any?
+    assert restaurant.errors[:location].any?
+  end    
 end
