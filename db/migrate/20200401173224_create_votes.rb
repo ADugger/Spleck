1d600b31e470ed62b6750a7f15d743cc6d1f0b54
Alan Dugger <adugger1@my.westga.edu>
2020-04-01 13:35:19 -0400

    Migrated vote model

---
+++ db/migrate/20200401173224_create_votes.rb
@@ -0,0 +1,11 @@
+class CreateVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :votes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
+      t.integer :votetype
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_135317) do
+ActiveRecord::Schema.define(version: 2020_04_01_173224) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_04_01_135317) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.integer "votetype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_votes_on_restaurant_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
