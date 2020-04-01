1050feb75ca5d7a50110e7553a62315a71326f66
Alan Dugger <adugger1@my.westga.edu>
2020-04-01 15:16:12 -0400

    Updated tests for redirect to sign in and for new vote controller methods

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @restaurant = restaurants(:one)
   end
@@ -28,32 +29,54 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should allow edit after login" do
+    sign_in users(:one)
     get edit_restaurant_url(@restaurant)
     assert_response :success
   end
+  
+  test "should redirect edit to sign in" do
+    get edit_restaurant_url(@restaurant)
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "should redirect update to sign in" do
+    patch restaurant_url(@restaurant), params: { restaurant: { downvotes: @restaurant.downvotes, location: @restaurant.location, name: @restaurant.name, upvotes: @restaurant.upvotes } }
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should update restaurant" do
+  test "should redirect destroy to sign in" do
+    assert_no_difference('Restaurant.count') do
+      delete restaurant_url(@restaurant)
+    end    
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "should update restaurant after login" do
+    sign_in users(:two)
     patch restaurant_url(@restaurant), params: { restaurant: { downvotes: @restaurant.downvotes, location: @restaurant.location, name: @restaurant.name, upvotes: @restaurant.upvotes } }
     assert_redirected_to restaurant_url(@restaurant)
   end
 
-  test "should destroy restaurant" do
+  test "should destroy restaurant after login" do
+    sign_in users(:one)
     assert_difference('Restaurant.count', -1) do
       delete restaurant_url(@restaurant)
     end    
     assert_redirected_to restaurants_url
   end
   
-  test "should increase upvotes of restaurant" do
-    assert_difference '@restaurant.reload.upvotes' do
+  test "should increase upvotes of restaurant after login" do
+    sign_in users(:one)
+    assert_difference '@restaurant.reload.votes.where(votetype: 1).count' do
       post upvote_url(@restaurant)
       assert_redirected_to restaurant_url(@restaurant)      
     end
   end
   
   test "should increase downvotes of restaurant" do
-    assert_difference '@restaurant.reload.downvotes' do
+    sign_in users(:one)
+    assert_difference '@restaurant.reload.votes.where(votetype: -1).count' do
       post downvote_url(@restaurant)
       assert_redirected_to restaurant_url(@restaurant)
     end
