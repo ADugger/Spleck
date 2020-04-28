37bb58fd45802146169c2ee014a7cb9874753364
Alan Dugger <adugger1@my.westga.edu>
2020-04-28 14:55:59 -0400

    Added comment viewing to account summary

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
   validates :body, presence: true
+  
+  def get_restaurant
+    @id = self.restaurant_id
+    @restaurants = Restaurant.where("id = ?", "#{@id}").first
+  end
 end
