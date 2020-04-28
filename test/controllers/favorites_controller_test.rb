be209603d3a0ff656a61dfb13c82662825e315f5
Alan Dugger <adugger1@my.westga.edu>
2020-04-28 13:27:50 -0400

    Added testing for favorites

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -1,48 +1,27 @@
 require 'test_helper'
 
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
+    @restaurant = restaurants(:one)
+    @user = users(:one)
+  end  
+  
+  test "should add a favorite to the collection" do
+    sign_in users(:one)
+    assert_not_includes(@restaurant.favorites, @favorite)    
     @favorite = favorites(:one)
+    post addfave_url(@user, @restaurant)
+    assert_includes(@restaurant.favorites, @favorite)
+    assert_redirected_to restaurant_url(@restaurant)
   end
-
-  test "should get index" do
-    get favorites_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_favorite_url
-    assert_response :success
-  end
-
-  test "should create favorite" do
-    assert_difference('Favorite.count') do
-      post favorites_url, params: { favorite: { restaurant_id: @favorite.restaurant_id, user_id: @favorite.user_id } }
-    end
-
-    assert_redirected_to favorite_url(Favorite.last)
-  end
-
-  test "should show favorite" do
-    get favorite_url(@favorite)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_favorite_url(@favorite)
-    assert_response :success
-  end
-
-  test "should update favorite" do
-    patch favorite_url(@favorite), params: { favorite: { restaurant_id: @favorite.restaurant_id, user_id: @favorite.user_id } }
-    assert_redirected_to favorite_url(@favorite)
-  end
-
-  test "should destroy favorite" do
-    assert_difference('Favorite.count', -1) do
-      delete favorite_url(@favorite)
-    end
-
-    assert_redirected_to favorites_url
+  
+  test "should remove a favorite from the collection" do
+    sign_in users(:one)
+    @favorite = favorites(:one)
+    assert_includes(@restaurant.favorites, @favorite)    
+    post removefave_url(@user, @restaurant)
+    assert_not_includes(@restaurant.favorites, @favorite)
+    assert_redirected_to restaurant_url(@restaurant)
   end
 end
--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @restaurant = restaurants(:two)
+    @favorite = favorites(:two)
+  end
+  
+  test "should get restaurant from the favorite collection" do
+    @retrieved_restaurant = @favorite.get_restaurant
+    assert_equal(@restaurant, @retrieved_restaurant)
+  end
 end
