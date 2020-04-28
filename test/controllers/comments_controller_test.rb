8c4730ffde0966907650bc10c73197fe2db7aa2d
Alan Dugger <adugger1@my.westga.edu>
2020-04-28 15:44:11 -0400

    Added tests for comment controller and model - FINAL SPRINT 3 COMMIT

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,48 +1,18 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
+    @restaurant = restaurants(:one)
+    @user = users(:one)
+  end  
+  
+  test "should add a comment to the collection" do
+    sign_in users(:one)
+    assert_not_includes(@restaurant.comments, @comment)    
     @comment = comments(:one)
-  end
-
-  test "should get index" do
-    get comments_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_comment_url
-    assert_response :success
-  end
-
-  test "should create comment" do
-    assert_difference('Comment.count') do
-      post comments_url, params: { comment: { body: @comment.body, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
-    end
-
-    assert_redirected_to comment_url(Comment.last)
-  end
-
-  test "should show comment" do
-    get comment_url(@comment)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_comment_url(@comment)
-    assert_response :success
-  end
-
-  test "should update comment" do
-    patch comment_url(@comment), params: { comment: { body: @comment.body, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
-    assert_redirected_to comment_url(@comment)
-  end
-
-  test "should destroy comment" do
-    assert_difference('Comment.count', -1) do
-      delete comment_url(@comment)
-    end
-
-    assert_redirected_to comments_url
-  end
+    post comment_url(@user, @restaurant)
+    assert_includes(@restaurant.comments, @comment)
+    assert_redirected_to restaurant_url(@restaurant)
+  end  
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @restaurant = restaurants(:two)
+    @comment = comments(:two)
+  end
+  
+  test "should get restaurant from collection of comments" do
+    @retrieved_restaurant = @comment.get_restaurant
+    assert_equal(@restaurant, @retrieved_restaurant)
+  end
 end
